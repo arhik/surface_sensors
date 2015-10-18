2e4b32bdb10e7dbc23b2ec7dbc28019efedeb049
arhik <kkatipally@umassd.edu>
2015-10-18 16:29:37 -0400

    removed hardcoded device id

--- Sensors.py
+++ Sensors.py
@@ -1,35 +1,41 @@
 
 import time
 
+
+
+
 import subprocess
 import bitstring
+import iio
+iioContext = iio.Context()
+devList = [ dev.name for dev in iioContext.devices ]
+print devList
+indx = devList.index('accel_3d')%(len(devList)/2)
+
 # from bitstring import BitArray
 
 def read_accel_x():
-	with open("/sys/bus/iio/devices/iio:device0/in_accel_x_raw","r") as accel_x:
+	with open("/sys/bus/iio/devices/iio:device{0}/in_accel_x_raw".format(indx),"r") as accel_x:
 		#x = BitArray(accel_x.read())
 		# sint = x.int
 		x = int(accel_x.read())
 		x = bitstring.Bits(uint=x,length=16)
 		[x] = x.unpack('int')
-
 	return (x)
 
 def read_accel_y():
-	with open("/sys/bus/iio/devices/iio:device0/in_accel_y_raw","r") as accel_y:
+	with open("/sys/bus/iio/devices/iio:device{0}/in_accel_y_raw".format(indx),"r") as accel_y:
 		y  = int(accel_y.read())
 		y = bitstring.Bits(uint=y,length=16)
 		[y] = y.unpack('int')
-
 	return y
 
 
 def read_accel_z():
-	with open("/sys/bus/iio/devices/iio:device0/in_accel_z_raw","r") as accel_z:
+	with open("/sys/bus/iio/devices/iio:device{0}/in_accel_z_raw".format(indx),"r") as accel_z:
 		z  = int(accel_z.read())
 		z = bitstring.Bits(uint=z,length=16)
 		[z] = z.unpack('int')
-
 	return z
 
 prev_left = False
@@ -59,7 +65,7 @@ while True:
 			subprocess.call(['xrandr','--output','eDP1','--rotate','normal'])
 			subprocess.call(['xinput', 'set-prop', 'NTRG0001:01 1B96:1B05 Pen', 'Coordinate Transformation Matrix', '1', '0', '0', '0', '1', '0', '0', '0', '1'])
 			subprocess.call(['xbacklight','-set','50'])
-	time.sleep(1)
+	time.sleep(2)
 	prev_left = left
 	prev_right = right
 	prev_normal = normal

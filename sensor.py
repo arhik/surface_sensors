f03d69ec54ef76cb3b2e42eb3753838b4ff37e09
arhik <kkatipally@umassd.edu>
2015-10-19 14:02:04 -0400

    Sensor simulation

---
+++ sensor.py
@@ -0,0 +1,24 @@
+import random
+
+import math
+
+import time
+
+class Sensor():
+	
+	
+	def __init__(self):
+		import math
+		import random
+		self.nxt_val_gen = self.sensor_gen()
+	
+	
+	def sensor_gen(self):
+		while(1):
+			rndm_val = random.random()
+			yield rndm_val
+	
+	def run(self):
+		while(1):
+			time.sleep(1)
+			print(self.nxt_val_gen.next())
